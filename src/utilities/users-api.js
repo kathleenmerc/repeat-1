7775e4b2f0b2f3f66d19f16e29b1af5819e3b895
Kathleen Mercado <kathleenmerc@gmail.com>
2022-12-22 18:59:17 -0500

    removed extra notes from files, and added code to save the token to localStorage

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,9 +1,3 @@
-/**
- * Utility modules: Modules that hold general purpose functions, for example, a formatTime(seconds)function. These modules are reusable in multiple projects.
- * Service modules: Service modules are where we can organize application specific logic such as functions for signing-up or logging in a user. Service modules often use and depend upon API modules.
- * API modules: API modules are for abstracting logic that make network requests such as AJAX calls to the backend or third-party APIs. This abstraction makes it easier to refactor code to use different techniques, libraries, etc. For example, we are going to be using fetch for our AJAX communications, however, refactoring to use a library such as Axios would be made easy thanks to the use of API modules.
- */
-
 // BASE PATH FOR THE EXPRESS ROUTE:
 const BASE_URL = '/api/users'
 
@@ -22,5 +16,4 @@ export async function signUpAPI(userData) {
         throw new Error('Invalid Sign Up')
     }
 
-    
 }
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -1,16 +1,11 @@
-/**
- * Utility modules: Modules that hold general purpose functions, for example, a formatTime(seconds)function. These modules are reusable in multiple projects.
- * Service modules: Service modules are where we can organize application specific logic such as functions for signing-up or logging in a user. Service modules often use and depend upon API modules.
- * API modules: API modules are for abstracting logic that make network requests such as AJAX calls to the backend or third-party APIs. This abstraction makes it easier to refactor code to use different techniques, libraries, etc. For example, we are going to be using fetch for our AJAX communications, however, refactoring to use a library such as Axios would be made easy thanks to the use of API modules.
- */
-
-// IMPORT ALL NAMED EXPORTS ATTACHED TO USERSAPI OBJECT (from utilities/users-api.js):
+// IMPORT ALL NAMED EXPORTS ATTACHED TO USERSAPI OBJECT:
 import * as usersAPI from './users-api'
 
 export async function signUpService(userData) {
-    // Need to delegate the network request code to the users-api.js API module, which will ultimately return a JSON Wed Token (JWT)
+    // Need to delegate the network request code to the users-api.js API module:
     const token = await usersAPI.signUpAPI(userData)
 
-    // for now, return what the server is sending back
+    // Save the token to localStorage:
+    localStorage.setItem("token", token)
     return token
 }
